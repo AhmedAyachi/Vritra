02e6dc68decbd40cd57323ccc9832c8405e896a9
AhmedAyachi <aayachi032@gmail.com>
2021-07-28 11:21:59 +0100

    to edit index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,13 @@
-
-
 declare module "vanilla";
+
+
+export const useRef:(startsWith:string)=>string;
+export const map:(array:any[],callback:(value:any,index:number,array:any)=>string)=>string;
+export const Router:(
+    target:HTMLElement,
+    routes:[{
+        hash:string,
+        memorize:boolean,
+        component:(props:{parent:HTMLElement})=>HTMLElement,
+    }]
+)=>void;
