cd37a7740c8200358b1114c884f6173e499dc6e0
AhmedAyachi <aayachi032@gmail.com>
2023-06-02 17:12:05 +0100

    update CherryView docs

--- src/CherryView/CherryView.d.ts
+++ src/CherryView/CherryView.d.ts
@@ -1,8 +1,8 @@
-import {ViewProps,ViewInterface as View} from "../index";
+import {ViewProps,ViewInterface as View} from "../../index";
 
 
 /**
  * Same as View component but with the default global cherry styling
  */
-export default function CherryView(props:ViewProps):View;
+export default function CherryView<Tag extends keyof HTMLElementTagNameMap|undefined=undefined>(props:ViewProps<Tag>):Tag extends undefined?View<"div">:View<Tag>;;
 
