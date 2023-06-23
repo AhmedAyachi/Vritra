1732574c29f841df1d97487d4f8fbbc241117f1b
AhmedAyachi <aayachi032@gmail.com>
2023-06-23 12:43:35 +0100

    View: Fragment tag

--- src/View/View.js
+++ src/View/View.js
@@ -3,26 +3,48 @@ import HtmlSanitizer from "./HtmlSanitizer";
 
 export default function View(props){
     const {parent,id,tag,className,at,style}=props;
-    const view=document.createElement(tag||"div");
+    const isFragment=(tag==="fragment");
+    const nodes=isFragment&&[];
+    const view=isFragment?document.createDocumentFragment():document.createElement(tag||"div");
     if(id){view.id=id};
     if(className){view.className=className};
     if(style){
         typeof(style)==="string"?view.setAttribute("style",style):Object.assign(view.style,style);
     }
-    parent&&((at==="start")?parent.insertAdjacentElement("afterbegin",view):parent.appendChild(view));
+    if((!isFragment)&&parent){
+        (at==="start")?parent.insertAdjacentElement("afterbegin",view):parent.appendChild(view);
+    }
+    
 
     Object.defineProperties(view,{
         innateHTML:{set:(html)=>{
-            view.innerHTML="";
+            if(!isFragment){view.innerHTML=""};
             view.beforeEndHTML=html;
+            parent?.appendChild(view);
         }},
         beforeEndHTML:{set:(html)=>{
             const sanitizedEl=HtmlSanitizer.sanitizeHtml(html,view);
-            sanitizedEl&&view.append(...sanitizedEl.childNodes);
+            if(sanitizedEl){
+                const childNodes=[...sanitizedEl.childNodes];
+                view.append(...childNodes);
+                if(isFragment){
+                    const lastNode=nodes[nodes.length-1];
+                    nodes.push(...childNodes);
+                    lastNode?.parentNode.insertBefore(view,lastNode.nextSibling);
+                }
+            }
         }},
         afterBeginHTML:{set:(html)=>{
             const sanitizedEl=HtmlSanitizer.sanitizeHtml(html,view);
-            sanitizedEl&&view.prepend(...sanitizedEl.childNodes);
+            if(sanitizedEl){
+                const childNodes=sanitizedEl.childNodes;
+                view.prepend(...childNodes);
+                if(isFragment){
+                    const firstNode=nodes[0]||parent;
+                    nodes.unshift(...childNodes);
+                    firstNode?.parentNode.insertBefore(view,firstNode);
+                }
+            }
         }},
         substitute:{value:(element)=>{
             view.replaceWith(element);
